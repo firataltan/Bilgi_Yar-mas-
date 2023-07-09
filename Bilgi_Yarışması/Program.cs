b866fded8687bbba591e10ab9f3d45aa46c4d327
firataltan <05210000939@ogrenci.ege.edu.tr>
2023-07-09 23:02:36 +0300

    Proje dosyası ekle.

---
+++ Bilgi_Yarışması/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilgi_Yarışması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Bilgi Yarışmasına Hoşgeldiniz ");
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine();
+
+            int soru = 1;
+            string cevap;
+            if (soru == 1) ;
+            {
+                Console.WriteLine("Türkiye'nin başkenti neresidir?");
+                Console.WriteLine();
+
+                Console.WriteLine("A) İstanbul ");
+                Console.WriteLine("B) Ankara ");
+                Console.WriteLine("C) İzmir ");
+                Console.WriteLine("D) Bursa ");
+                Console.WriteLine();
+                Console.Write(" Cevabınız: ");
+                cevap = Console.ReadLine();
+
+                if (cevap == "b" || cevap == "B")
+                {
+                    soru = soru + 1;
+
+
+                }
+                else
+                {
+                    Console.Write("Cevap Yanlış Toplam Puan: 0 ");
+
+                }
+            }
+            if (soru==2)
+            {
+                Console.WriteLine("Cumhuriyet kaç yılında ilan edildi? ");
+                Console.WriteLine();
+                Console.WriteLine("A) 1920");
+                Console.WriteLine("B) 1924");
+                Console.WriteLine("C) 1925");
+                Console.WriteLine("D) 1923");
+                Console.WriteLine();
+                Console.Write("Cevabınız: ");
+                cevap = Console.ReadLine();
+                if(cevap=="d" || cevap == "D")
+                {
+                    soru = soru + 1;
+
+                }
+                else
+                {
+                    Console.Write("Yanlış cevap toplam puanınız: 1");
+
+                }
+                        
+                
+
+            }
+            if (soru == 3)
+            {
+                Console.WriteLine("2020-2021 Sezonu Spor toto süper lig şampiyonu hangi takımdır ? ");
+                Console.WriteLine();
+                Console.WriteLine("A) Beşiktaş");
+                Console.WriteLine("B) fenerbahçe");
+                Console.WriteLine("C) galatasaray");
+                Console.WriteLine("D) Trabzonspor");
+                Console.WriteLine();
+                Console.Write("Cevabınız: ");
+                cevap = Console.ReadLine();
+                if (cevap == "a" || cevap == "A")
+                {
+                    Console.Write("Tebrikler bütün soruları doğru cevapladınız");
+
+                }
+                else
+                {
+                    Console.Write("Yanlış cevap toplam puanınız: 2");
+
+                }
+
+
+
+            }
+
+
+
+
+
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Bilgi_Yarışması/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Bilgi_Yarışması")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Bilgi_Yarışması")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("88f8edde-5cea-4ab1-9b49-5959fcc9ee09")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
